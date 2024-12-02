23bf9304c90f611a304b73ea478e2707565dc057
Boris Gazur <barlukh@gmail.com>
2024-12-02 14:18:15 +0200

    Add chessboard

--- main.py
+++ main.py
@@ -2,11 +2,40 @@
 
 import pygame
 
-class Square(pygame.sprite.Sprite):
+class Board(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load('graphics/squares/square_brown_dark.png').convert
-        self.rect = self.image.get_rect(topleft = (0, 0))
+        self.squares = []
+        self.matrix = {}
+    
+    def alternate(self):
+        while True:
+          yield 1
+          yield 0
+
+    def draw_matrix(self):
+        y = -50
+        colour_change = self.alternate()
+        for number in "87654321":
+            colour = next(colour_change)
+            x = 120
+            y += 70
+            for letter in "abcdefgh":
+                rectangle = self.squares[colour].get_rect(topleft = (x, y))
+                self.matrix[letter + number] = (rectangle, colour)
+                screen.blit(self.squares[colour], (x, y))
+                colour = next(colour_change)
+                x += 70
+    
+    def load_squares(self):
+        for name in ['square_brown_dark.png', 'square_brown_light.png']:
+            square = pygame.image.load(f'graphics/squares/{name}').convert()
+            square = pygame.transform.scale(square, (70, 70))
+            self.squares.append(square)
+
+    def draw_board(self):
+        self.load_squares()
+        self.draw_matrix()
 
 pygame.init()
 screen = pygame.display.set_mode((800, 600))
@@ -15,12 +44,7 @@ pygame.display.set_caption('Chess')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 
-square_dark = pygame.image.load('graphics/squares/square_brown_dark.png')
-square_dark = pygame.transform.scale(square_dark, (70, 70))
-square_light = pygame.image.load('graphics/squares/square_brown_light.png')
-square_light = pygame.transform.scale(square_light, (70, 70))
-
-square_matrix = []
+chessboard = Board()
 
 while True:
     for event in pygame.event.get():
@@ -28,7 +52,7 @@ while True:
             pygame.quit
             exit()
 
-    screen.blit(square_dark, (0, 0))
+    chessboard.draw_board()
 
     pygame.display.update()
     clock.tick(60)
