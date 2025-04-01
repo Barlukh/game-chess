b856373cf8f7a6f5b38a32c1663a957aac32a005
Boris Gazur <barlukh@gmail.com>
2025-04-01 20:50:18 +0300

    Create main function

--- main.py
+++ main.py
@@ -20,31 +20,53 @@ icon = pygame.image.load('graphics/pieces/b_king.png')
 pygame.display.set_caption('Chess')
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
+pieces = pygame.sprite.Group()
+squares = pygame.sprite.Group()
+selected = pygame.sprite.GroupSingle()
+chessboard = pygame.image.load('graphics/squares/chessboard.png')
+board_rect = pygame.Rect(117, 16, 568, 568)
 
-def check_overlap():
-    """ Check for a collision of pieces. Return bool. """
-    overlap = False
-    for piece in pieces:
-        if piece.rect.colliderect(selected.sprite.rect):
-            overlap = True
-    return overlap
+def main():
+    picked_status = False
+    create_board()
+    create_pieces()
 
-def get_mouse():
-    """ Get and return a tuple of x and y position of the cursor. """
-    mouse_pos = pygame.mouse.get_pos()
-    mouse_x = mouse_pos[0]
-    mouse_y = mouse_pos[1]
-    return (mouse_x, mouse_y)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit
+                exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if board_rect.collidepoint(get_mouse()):
+                    if picked_status == True:
+                        if check_overlap() == True:
+                            selected.sprite.rect.center = original.rect.center
+                            selected.sprite.add(pieces)
+                            selected.sprite.remove(selected)
+                        else:
+                            for square in squares:
+                                if square.rect.collidepoint(get_mouse()):
+                                    selected.sprite.rect.center = square.rect.center
+                                    selected.sprite.add(pieces)
+                                    selected.sprite.remove(selected)
+                        picked_status = False
+                    else:
+                        for piece in pieces:
+                            if piece.rect.collidepoint(get_mouse()):
+                                piece.remove(pieces)
+                                piece.add(selected)
+                                original = copy.copy(piece)
+                                picked_status = True
+        
+        if len(selected) != 0:
+            selected.sprite.rect = selected.sprite.image.get_rect(center = (get_mouse()))
+        
+        screen.blit(chessboard, (0, 0))
+        pieces.draw(screen)
+        selected.draw(screen)
 
-def create_pieces():
-    """ Create a sprite.Group for all the pieces with their starting positions. """
-    starting_positions = coordinates.create_positions()
-    board_matrix = coordinates.create_matrix()
-    for key, values in starting_positions.items():  
-        for value in values:
-            x = board_matrix[value][0]
-            y = board_matrix[value][1]
-            pieces.add(classes.Piece(x, y, key))
+        pygame.display.update()
+        clock.tick(60)
 
 def create_board():
     """ Create a sprite.Group for all the squares of the chessboard and save the image. """
@@ -57,49 +79,30 @@ def create_board():
     squares.draw(screen)
     pygame.image.save(screen, 'graphics/squares/chessboard.png')
 
-pieces = pygame.sprite.Group()
-squares = pygame.sprite.Group()
-selected = pygame.sprite.GroupSingle()
-create_board()
-create_pieces()
+def create_pieces():
+    """ Create a sprite.Group for all the pieces with their starting positions. """
+    starting_positions = coordinates.create_positions()
+    board_matrix = coordinates.create_matrix()
+    for key, values in starting_positions.items():  
+        for value in values:
+            x = board_matrix[value][0]
+            y = board_matrix[value][1]
+            pieces.add(classes.Piece(x, y, key))
 
-chessboard = pygame.image.load('graphics/squares/chessboard.png')
-board_rect = pygame.Rect(117, 16, 568, 568)
-picked_status = False
+def get_mouse():
+    """ Get and return a tuple of x and y position of the cursor. """
+    mouse_pos = pygame.mouse.get_pos()
+    mouse_x = mouse_pos[0]
+    mouse_y = mouse_pos[1]
+    return (mouse_x, mouse_y)
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit
-            exit()
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if board_rect.collidepoint(get_mouse()):
-                if picked_status == True:
-                    if check_overlap() == True:
-                        selected.sprite.rect.center = original.rect.center
-                        selected.sprite.add(pieces)
-                        selected.sprite.remove(selected)
-                    else:
-                        for square in squares:
-                            if square.rect.collidepoint(get_mouse()):
-                                selected.sprite.rect.center = square.rect.center
-                                selected.sprite.add(pieces)
-                                selected.sprite.remove(selected)
-                    picked_status = False
-                else:
-                    for piece in pieces:
-                        if piece.rect.collidepoint(get_mouse()):
-                            piece.remove(pieces)
-                            piece.add(selected)
-                            original = copy.copy(piece)
-                            picked_status = True
-    
-    if len(selected) != 0:
-        selected.sprite.rect = selected.sprite.image.get_rect(center = (get_mouse()))
-    
-    screen.blit(chessboard, (0, 0))
-    pieces.draw(screen)
-    selected.draw(screen)
+def check_overlap():
+    """ Check for a collision of pieces. Return bool. """
+    overlap = False
+    for piece in pieces:
+        if piece.rect.colliderect(selected.sprite.rect):
+            overlap = True
+    return overlap
 
-    pygame.display.update()
-    clock.tick(60)
+if __name__ == "__main__":
+    main()
